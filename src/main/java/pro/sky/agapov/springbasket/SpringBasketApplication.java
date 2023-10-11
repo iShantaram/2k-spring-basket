1f55d16e870435f2434f91783489916806ba8f88
iShantaram <1agapovav987@gmail.com>
2023-10-11 13:15:01 +0300

    Init commit

---
+++ src/main/java/pro/sky/agapov/springbasket/SpringBasketApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.agapov.springbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/agapov/springbasket/SpringBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.agapov.springbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
